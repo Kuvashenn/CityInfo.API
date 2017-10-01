201049da1bab497ef72e62e80b416993f4b979d8
Kuvashenn <kuvashen@hotmail.com>
2017-10-01 19:18:35 +0200

    Worked on EF core

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CityInfo.API.Services;
+using CityInfo.API.Models;
 
 namespace CityInfo.API.Controllers
 {
@@ -11,11 +13,33 @@ namespace CityInfo.API.Controllers
     [Route("api/Cities")]
     public class CitiesController : Controller
     {
+      
+        private ICityInfoRepository _cityInfoRepository;
+
+        public CitiesController(ICityInfoRepository cityInfoRepository)
+        {
+            _cityInfoRepository = cityInfoRepository;
+        }
+
         [HttpGet]
         public ActionResult GetCities()
         {
-            return Ok(CitiesDataStore.Current.Cities);
-           
+            //return Ok(CitiesDataStore.Current.Cities);
+            var cityEntities = _cityInfoRepository.GetCities();
+
+            var results  = new List<CityWithoutPointsOfInterstDto>();
+
+            foreach (var cityEntity in cityEntities)
+            {
+                results.Add(new CityWithoutPointsOfInterstDto
+                {
+                    Id = cityEntity.Id,
+                    Description = cityEntity.Description,
+                    Name = cityEntity.Name
+                });
+            }
+
+            return Ok(results);
         }
 
         [HttpGet("{id}")]
